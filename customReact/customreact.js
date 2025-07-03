5b80209c0793679ccc00f99b516ce8e27eb079cb
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-04 01:09:33 +0530

    js change

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,11 +1,19 @@
 function customRender(reactElement, container) {
-  const domElement = document.createElement(reactElement.type);
-  domElement.innerHtml = reactElement.children;
-  domElement.setAttribute("href", reactElement.props.href);
-  domElement.setAttribute("target", reactElement.props.target);
-}
+  //   const domElement = document.createElement(reactElement.type);
+  //   domElement.innerHTML = reactElement.children;
+  //   domElement.setAttribute("href", reactElement.props.href);
+  //   domElement.setAttribute("target", reactElement.props.target);
 
-import { Children } from "react";
+  //   container.appendChild(domElement);
+
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  for (const prop in reactElement.props) {
+    if (prop == "children") continue;
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  container.appendChild(domElement);
+}
 
 const reactElement = {
   type: "a",
